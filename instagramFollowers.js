40aec40224be3a362271241b897c0b717b9f1e1d
Sidmaz666 <sidmazumdr8@gmail.com>
2023-06-22 10:03:19 +0530

    Implementing Proper Scroll & Follow Function  🚀

--- instagramFollowers.js
+++ instagramFollowers.js
@@ -10,73 +10,74 @@
  *
  * */
 
+let isScroll = true
+
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-(async function () {
-  let totalFollowing = Number(
-    document
-      .querySelector("section > ul > li:nth-child(3)")
-      .textContent.replace(/following/, "")
-      .trim()
-  );
-
-  if(totalFollowing > 200) totalFollowing = 200
-
-  let sP = 385;
-
-  console.log("%cPlease Wait!", "color:green;font-style:bold;font-size:22px;");
-
-  while (true) {
-    if (
-      document.querySelectorAll(
-        '[role="dialog"] > div > div > div > div > ._aano > div > div > div'
-      ).length == totalFollowing
-    ) {
-      break;
-    }
-    document
-      .querySelector('[role="dialog"] > div > div > div > div > ._aano')
-      .scrollTo(0, sP);
-    await sleep(3000);
-    sP += sP;
+async function scrollAndDetectChildNodes() {
+  const parentElement = document.querySelector('[role="dialog"] > div > div > div > div > ._aano');
+  parentElement.scrollTop = parentElement.scrollHeight;
+  await sleep(5000)
+  const childNodes = parentElement.children[0].children[0].childNodes;
+  const addedChildNodes = Array.from(childNodes).filter(node => node.nodeType === Node.ELEMENT_NODE);
+  if (addedChildNodes.length > 0) {
+    isScroll=true
+  } else {
+    isScroll=false
   }
+}
 
-  let totalListLength = document.querySelectorAll(
-    '[role="dialog"] > div > div > div > div > ._aano > div > div > div'
-  );
-  let limit = 0;
-  for (let i = 0; i < 200; i++) {
-    const d = totalListLength[i];
-    const followBtn = d.querySelector("button");
-    if (followBtn.textContent == "Follow") {
-      followBtn.click();
+async function Follow(){
+let totalListLength = document.querySelectorAll(
+  '[role="dialog"] > div > div > div > div > ._aano > div > div > div'
+);
+let limit = 0;
+let followed = 1;
+for (let i = 0; i < totalListLength.length; i++) {
+  const d = totalListLength[i];
+  const followBtn = d.querySelector("button");
+  if (followBtn.textContent == "Follow") {
+    followBtn.click();
+    console.log(
+      `%cFollowed ${d
+	.querySelector("span > span")
+	.textContent.replace(/Verified/g, "")
+	.trim()}`,
+      "color:cyan;font-style:bold;font-size:18px;"
+    );
+    limit++;
+    followed++;
+    if (limit == 5) {
       console.log(
-        `%cFollowed ${d
-          .querySelector("span > span")
-          .textContent.replace(/Verified/g, "")
-          .trim()}`,
-        "color:cyan;font-style:bold;font-size:18px;"
+	"%cSleeping For 5min!",
+	"color:orange;font-style:bold;font-size:20px;"
       );
-      limit++;
-      if (limit == 5) {
-        console.log(
-          "%cSleeping For 5min!",
-          "color:orange;font-style:bold;font-size:20px;"
-        );
-        limit = 0;
-        await sleep(300000);
-      }
-      if (i == 200) {
-        console.log("Reach Limit For a Day!");
-        return;
-      }
+      limit = 0;
+      await sleep(300000);
     }
+    if (followed == 200) {
+      console.log("Reach Limit For a Day!");
+      return;
+   }
   }
+ }
+}
+
+
+(async function () {
+  console.log("%cPlease Wait!", "color:green;font-style:bold;font-size:22px;");
+  while(true){
+    	if(!isScroll) break;
+	await scrollAndDetectChildNodes()
+  }
+
+  await Follow()
+
 })().then(() => {
-        console.log(
-          "%cDone Following!",
-          "color:green;font-style:bold;font-size:22px;"
-        );
+  console.log(
+    "%cDone Following!",
+    "color:green;font-style:bold;font-size:22px;"
+  );
 });
